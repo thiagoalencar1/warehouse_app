1c915f401d2e4a52c7b8dca1dc6b9376b06b173a
Thiago Alencar <talencar@gmail.com>
2022-05-01 09:57:27 -0300

    fix: typing correction: 'confir to config' in 'spec/rails_helper.rb'.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  confir.before(type: :system) do
+  config.before(type: :system) do
     driven_by(:rack_test)
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
