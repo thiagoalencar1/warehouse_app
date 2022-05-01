cd40565eec732d4c61ce6019810c8cff7841718d
Thiago Alencar <talencar@gmail.com>
2022-05-01 13:23:47 -0300

    test: added more two tests
    - Added tests: 'e vê galpões cadastrados' and 'e não existem galpões cadastrados'.

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'User visit initial page' do
-  it 'and sees app name' do
+describe 'Usuário visita tela inicial' do
+  it 'e vê nome da aplicação' do
     # Arrange
 
     # Act
@@ -9,5 +9,36 @@ describe 'User visit initial page' do
 
     # Assert
     expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'e vê galpões cadastrados' do
+    # Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000)
+
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceió')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceió')
+    expect(page).to have_content('50000 m²')
+  end
+
+  it 'e não existem galpões cadastrados' do
+    # Arrange
+
+    # Act
+    visit('/')
+
+    # Assert
+    expect(page).to have_content('Não existem galpões cadastrados')
   end
 end
