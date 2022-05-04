ee8d73d0a21ca1e64f36336d088c09dc85ee8d11
Thiago Alencar <talencar@gmail.com>
2022-05-04 09:41:30 -0300

    feat: feita a página que mostra detalhes de um galpão - Adicionados os atributos address, cep e description ao model Warehouse. - Rota, controller e view preparadas para acessar detalhes do galpão.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_01_151424) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_120102) do
   create_table "warehouses", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_01_151424) do
     t.integer "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "cep"
+    t.string "description"
   end
 
 end
