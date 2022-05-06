f820e95cf5afc57aa4c1ab44ec32245de5a9cb84
Thiago Alencar <talencar@gmail.com>
2022-05-06 14:09:12 -0300

    Validação de CEP.

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,5 @@
 class Warehouse < ApplicationRecord
   validates :name, :code, :city, :description, :address, :cep, :area, presence: true
   validates :code, uniqueness: true
+  validates :cep, format: { with: /[0-9]{5}-[0-9]{3}/, message: 'formato de cep invalido' }
 end
