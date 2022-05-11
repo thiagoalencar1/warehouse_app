c81a167c4e50d52dbc4eea5242e786d12179c86c
Thiago Alencar <talencar@gmail.com>
2022-05-10 22:06:08 -0300

    fix: Corrige validações do cadastro de Fornecedor.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,7 +1,8 @@
 class Supplier < ApplicationRecord
   validates :corporate_name, :brand_name, :email, :nif, presence: true
   validates :nif, uniqueness: true
-  validates :nif, length: { is: 13, message: 'precisa ter 13 digitos.' }
+  validates :nif, length: { is: 13, message: 'deve ter 13 digitos.' }
   validates :nif, format: { with: /\A\d{13}\z/, message: 'só aceita números.' }
   validates :email, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/, message: 'com formato inválido.' }
+  validates :state, length: { is: 2, message: 'deve ter 2 dígitos.' }
 end
